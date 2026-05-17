d972445bfd55f3a949520739334b70e41bdfc6b7
Sushant <khanalsushan14@gmail.com>
2026-05-17 14:19:51 +0545

    basic template of vite and to do list structure created in jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+  <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">To Do List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 2
+        </label>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 3
+        </label>
+      </li>
+    </ul>
+
+  </>
+  )
+}
