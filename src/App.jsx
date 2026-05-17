d69ebefe9b00bd719298d2973f76172372061267
Sushant <khanalsushan14@gmail.com>
2026-05-17 15:57:24 +0545

    ToDo logic created with different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,54 @@
+import { NewTodoForm } from "./NewTodoForm";
 import "./styles.css"
+import { useEffect, useState } from "react"
+import { TodoList } from "./TodoList";
 
 export default function App() {
+
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
+
+ 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title){
+    setTodos(currentTodos =>{
+      return [
+                ...currentTodos,
+                {id: crypto.randomUUID(), title, completed: false},
+              ]
+    })
+  }
+
+
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
   <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <NewTodoForm onSubmit={addTodo} />
     <h1 className="header">To Do List</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 2
-        </label>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 3
-        </label>
-      </li>
-    </ul>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
 
   </>
   )
