15a46f7e05706f588eeba266e14f3ea2fe7e887b
Sushant <khanalsushan14@gmail.com>
2026-05-17 16:12:22 +0545

    final touch

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,26 @@
 import { TodoItem } from "./TodoItem";
 
-export function TodoList({todos, toggleTodo, deleteTodo}) {
-    return(
-            <ul className="list">
-                {todos.length === 0 && "No Todos"}
-                {todos.map(todo => {
-                    return (
-                        <TodoItem 
-                            {...todo}
-                            key={todo.id}
-                            toggleTodo={toggleTodo}
-                            deleteTodo={deleteTodo} 
-                        /> 
-          
-                    )
-      
-                })}    
-    
-            </ul>
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  if (todos.length === 0) {
+    return (
+      <div className="empty-state">
+        <span className="empty-icon">📋</span>
+        <p className="empty-title">No tasks yet</p>
+        <p className="empty-sub">Add something above to get started</p>
+      </div>
+    );
+  }
 
-    )
-}    
-   
+  return (
+    <ul className="list">
+      {todos.map(todo => (
+        <TodoItem
+          {...todo}
+          key={todo.id}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
